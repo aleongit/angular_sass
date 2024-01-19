bd9c3150bb960f1d31e551a459e356703d38901e
aleongit <aleix.leon@gmail.com>
2024-01-19 14:59:07 +0100

    feat: configure ngx-markdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MarkdownModule, provideMarkdown } from 'ngx-markdown';
+import { HttpClient } from '@angular/common/http';
+import { SecurityContext } from '@angular/core';
+
+// enable default sanitization
+provideMarkdown();
+
+// turn off sanitization
+provideMarkdown({
+  sanitize: SecurityContext.NONE,
+});
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MarkdownModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [provideMarkdown({ loader: HttpClient })],
 })
 export class AppComponent {
   title = 'angular_sass';
